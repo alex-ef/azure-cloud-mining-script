14eee0794e0cd8c0ed684f78ce28c9b693d8225b
alex-ef <46449322+alex-ef@users.noreply.github.com>
2019-12-06 20:35:43 +0200

    Updating to use XmrRig

--- azure_script/compile_and_config.sh
+++ azure_script/compile_and_config.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-
-
-cmake . -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
-make
-
-azure_script/create_cpu_config.sh > bin/cpu.txt
-azure_script/create_pool_config.sh > bin/pools.txt
-cp azure_script/config.txt bin/config.txt
-cp azure_script/config11.txt bin/config11.txt
+cd ..
+rm -rf xmrig/
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+git checkout v5.0.1
+mkdir build
+cd build
+cmake ..
+make 
+cd ..
+cd ..
